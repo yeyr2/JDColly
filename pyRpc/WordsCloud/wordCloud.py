061cc84c978f2602feadd9dd75951617eafdf85b
lql-arch <1846001028@qq.com>
2023-04-04 23:00:45 +0800

    fixed python grpc service and go grpc client

--- pyRpc/WordsCloud/wordCloud.py
+++ pyRpc/WordsCloud/wordCloud.py
@@ -40,8 +40,8 @@ def word_cloud(text_str):
     stopwords = set(map(str.strip, open('../stopwords/cn_stopwords.txt').readlines()))
     wc = WordCloud(background_color="white",  # 设置背景颜色
                    max_words=2000,  # 词云显示的最大词数
-                   height=800,  # 图片高度
-                   width=800,  # 图片宽度
+                   height=400,  # 图片高度
+                   width=400,  # 图片宽度
                    max_font_size=50,  # 最大字体
                    stopwords=stopwords,  # 设置停用词
                    font_path=font_path,  # 兼容中文字体，不然中文会显示乱码
@@ -54,8 +54,8 @@ def word_cloud(text_str):
         print(f"Folder {folder_name} has been created.")
     else:
         print(f"Folder {folder_name} already exists.")
-    paths = "../images/1.jpg"
-    wc.to_file(paths)
+    paths = "1.jpg"
+    wc.to_file("../images/"+paths)
 
     # 显示图像
     # plt.imshow(wc, interpolation='bilinear')
