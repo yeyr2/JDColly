f8043e067082d70697fe2e9e4046f6c277919027
lql-arch <1846001028@qq.com>
2023-04-11 16:22:03 +0800

    fixed service.py

--- pyAnalyzeComment/service.py
+++ pyAnalyzeComment/service.py
@@ -13,7 +13,7 @@ class AnalyzeServicer(analyze_pb2_grpc.GreeterServicer):
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     analyze_pb2_grpc.add_GreeterServicer_to_server(AnalyzeServicer(), server)
-    server.add_insecure_port('[::]:50052')
+    server.add_insecure_port('pyAnalyzeComment:50052')
     server.start()
     server.wait_for_termination()
 
--- pyWordCloud/service.py
+++ pyWordCloud/service.py
@@ -19,7 +19,7 @@ class GreeterServicer(wordCloud_pb2_grpc.GreeterServicer):
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     wordCloud_pb2_grpc.add_GreeterServicer_to_server(GreeterServicer(), server)
-    server.add_insecure_port('[::]:50051')
+    server.add_insecure_port('pyWordCloud:50051')
     server.start()
     server.wait_for_termination()
 
